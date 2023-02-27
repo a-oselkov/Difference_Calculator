447226432348d5b7bc0f3db86b2085253f04dab7
a-oselkov <a.oselkov@gmail.com>
2023-02-27 22:39:24 +0300

    step 8

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -7,23 +7,18 @@ public class Stylish {
     public static String mapToString(Map<String, Object> map) {
         return " " + map.get("key") + ": " + map.get("value") + "\n";
     }
-
     public static String toStylishFormat(List<Map<String, Object>> differenceList) {
         StringBuilder result = new StringBuilder("{\n");
 
         for (Map<String, Object> map : differenceList) {
             if (map.get("status").equals("added")) {
-                result.append("  +").append(" ").append(map.get("key")).append(": ")
-                        .append(map.get("value")).append("\n");
+                result.append("  +").append(mapToString(map));
             } else if (map.get("status").equals("removed")) {
-                result.append("  -").append(" ").append(map.get("key")).append(": ")
-                        .append(map.get("value")).append("\n");
+                result.append("  -").append(mapToString(map));
             } else if (map.get("status").equals("unchanged")) {
-                result.append("   ").append(" ").append(map.get("key")).append(": ")
-                        .append(map.get("value")).append("\n");
+                result.append("   ").append(mapToString(map));
             } else {
-                result.append("  -").append(" ").append(map.get("key")).append(": ")
-                        .append(map.get("value")).append("\n");
+                result.append("  -").append(mapToString(map));
                 result.append("  + ").append(map.get("key")).append(": ").append(map.get("valueNew")).append("\n");
             }
         }
