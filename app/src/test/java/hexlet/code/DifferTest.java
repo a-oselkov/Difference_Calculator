b0a44eb74dcb1727ff15b114e1f418e2ad3b6915
a-oselkov <a.oselkov@gmail.com>
2023-03-01 23:53:09 +0300

    update project

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -13,6 +13,8 @@ import java.nio.file.Paths;
 class DifferTest {
     private final String jsonFile1 = "src/test/resources/file1.json";
     private final String jsonFile2 = "src/test/resources/file2.json";
+    private final String yamlFile1 = "src/test/resources/file1.yaml";
+    private final String yamlFile2 = "src/test/resources/file2.yaml";
     private static final Path STYLISPATH = Paths.get("src/test/resources/stylish-test.txt")
             .toAbsolutePath().normalize();
     private static final Path PLAINPATH = Paths.get("src/test/resources/plain-test.txt")
@@ -22,29 +24,32 @@ class DifferTest {
     private static String stylish;
     private static String plain;
     private static String json;
+
     @BeforeAll
     static void readResult() throws IOException {
         stylish = Files.readString(STYLISPATH).trim();
         plain = Files.readString(PLAINPATH).trim();
         json = Files.readString(JSONPATH).trim();
     }
+
     @Test
     void generateTest1() throws IOException {
         String actual = Differ.generate(jsonFile1, jsonFile2);
         assertEquals(stylish, actual);
     }
+
     @Test
     void generateTest2() throws IOException {
-        String yamlFile1 = "src/test/resources/file1.yaml";
-        String yamlFile2 = "src/test/resources/file2.yaml";
         String actual = Differ.generate(yamlFile1, yamlFile2);
         assertEquals(stylish, actual);
     }
+
     @Test
     void generateTest3() throws IOException {
         String actual = Differ.generate(jsonFile1, jsonFile2, "plain");
         assertEquals(plain, actual);
     }
+
     @Test
     void generateTest4() throws IOException {
         String actual = Differ.generate(jsonFile1, jsonFile2, "json");
