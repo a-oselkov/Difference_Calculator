da239622f8317d35b2fd6a322baebc3e24c96692
a-oselkov <a.oselkov@gmail.com>
2023-03-01 22:02:47 +0300

    update project

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -13,12 +13,11 @@ import java.util.Map;
 public class Parser {
 
     public static Map<String, Object> fileToMap(String filePath) throws IOException {
-        final int fileFormatLength = 5;
-        String fileType = filePath.substring(filePath.length() - fileFormatLength);
-        ObjectMapper objectMapper = fileType.equals("json") ? new ObjectMapper() : new ObjectMapper(new YAMLFactory());
+        String fileType = filePath.substring(filePath.lastIndexOf("."));
+        ObjectMapper mapper = fileType.equals("json") ? new ObjectMapper() : new ObjectMapper(new YAMLFactory());
         Path path = Paths.get(filePath).toAbsolutePath().normalize();
         String dataFile = Files.readString(path);
-        Map<String, Object> data = objectMapper.readValue(dataFile, new TypeReference<Map<String, Object>>() { });
+        Map<String, Object> data = mapper.readValue(dataFile, new TypeReference<Map<String, Object>>() { });
         return data;
     }
 }
