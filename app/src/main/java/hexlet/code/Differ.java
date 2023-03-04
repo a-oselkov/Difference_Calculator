8288c3f29f679e848af44b77b7fcb9480e4cf52a
a-oselkov <a.oselkov@gmail.com>
2023-03-04 15:42:13 +0300

    update project

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -11,18 +11,25 @@ public class Differ {
     public static String generate(String filePath1, String filePath2, String format) throws IOException {
         Map<String, Object> parsingDataFile1 = getData(filePath1);
         Map<String, Object> parsingDataFile2 = getData(filePath2);
+
         List<Map<String, Object>> differences = Differences.getDifferencesList(parsingDataFile1, parsingDataFile2);
+
         return Formatter.makeFormat(differences, format);
     }
+
     public static String generate(String fistFilePath, String secondFilePath) throws IOException {
         return generate(fistFilePath, secondFilePath, "stylish");
     }
+
     public static Map<String, Object> getData(String filePath) throws IOException {
         Path path = Paths.get(filePath).toAbsolutePath().normalize();
+
         String dataFile = Files.readString(path);
         String extension = getFileExtension(filePath);
+
         return Parser.parse(dataFile, extension);
     }
+
     public static String getFileExtension(String filePath) {
         String[] split = filePath.split("\\.");
         return split[split.length - 1];
