5c0cbdb6504873490a3024af730a1da3debbda38
a-oselkov <a.oselkov@gmail.com>
2023-03-17 13:37:03 +0300

    update project

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.io.IOException;
 import java.util.Map;
 
 public class Parser {
